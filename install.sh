55a30cfdb8663c228587e3eb5a3cf54016c829e6
Lucas Maurice <lucas.maurice@outlook.com>
2018-10-12 07:00:43 -0400

    Add mediawiki installation and requirements.

--- install.sh
+++ install.sh
@@ -21,10 +21,16 @@ sudo mysql_secure_installation
 
 echo -e "${GREEN}Installer:${NC} Will install PHP"
 
-sudo apt install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php-common php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline
+sudo apt install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php-common php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline php7.0-mbstring php7.0-xml
 sudo a2enmod php7.0
 
 sudo systemctl restart apache2
 sudo systemctl enable apache2
 
+echo -e "${GREEN}Installer:${NC} Will install MediaWiki"
+
 cd /var/www/html/
+sudo wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
+sudo tar xvzf mediawiki-*.tar.gz
+sudo cp ./mediawiki-*/* ./ --recursive
+sudo rm ./mediawiki-* -rf
